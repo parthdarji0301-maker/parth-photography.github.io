3b90101fd3fd11bb97cfa915105d8c15287b8103
parthdarji0301-maker <parthdarji0301@gmail.com>
2026-06-19 16:33:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// Mobile menu toggle
+const menuToggle = document.querySelector('.menu-toggle');
+const mobileMenu = document.getElementById('mobileMenu');
+
+if (menuToggle && mobileMenu) {
+  menuToggle.addEventListener('click', () => {
+    mobileMenu.classList.toggle('open');
+  });
+
+  mobileMenu.querySelectorAll('a').forEach(link => {
+    link.addEventListener('click', () => {
+      mobileMenu.classList.remove('open');
+    });
+  });
+}
